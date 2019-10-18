64556648da6610737e1d0f48157b27343f80f33f
Damian <d.pagowski@gmail.com>
2019-10-18 19:16:17 +0700

    rate limiting applied

--- app.js
+++ app.js
@@ -18,6 +18,22 @@ app.use(function middleware (req, res, next) {
   next()
 })
 
+
+// rate limit
+const rateLimit = require("express-rate-limit");
+ 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+ 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests
+});
+ 
+//  apply to all requests
+app.use(limiter);
+
 // serve static files
 app.use('/public', express.static(process.cwd() + '/public'))
 
