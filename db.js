6e9fef28c8c27bb210c50f98eb16ff4db1962cb2
Damian <d.pagowski@gmail.com>
2019-10-18 12:09:40 +0700

    support for connecting hosted db

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const database = process.env.DB_NAME
 const server = process.env.DB_SERVER_ADDR
 const dbPort = process.env.DB_PORT
+const uri = process.env.MONGOLAB_URI || `mongodb://${server}:${dbPort}/${database}`
 console.log(
-  'Connecting to database - ' + `mongodb://${server}:${dbPort}/${database}`
+  `Connecting to database: ${process.env.MONGOLAB_URI ? "MLAB" : uri}`
 )
-mongoose.connect(`mongodb://${server}:${dbPort}/${database}`)
+mongoose.connect(uri)
